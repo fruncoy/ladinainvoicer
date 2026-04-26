0cdeac3ee6048c25662b5b183bfacab0ad8b7119
fruncoy <fruncoy@users.noreply.github.com>
2026-04-27 00:23:37 +0300

    Fix PrismaClient import for ESM compatibility

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
